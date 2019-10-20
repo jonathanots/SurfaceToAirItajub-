e56d79bb96f38551fc62c3680691924d3820ec77
Rodrigo Porto <rodrigoandradeporto@hotmail.com>
2019-10-20 02:58:14 -0300

    Correção dos atributos "latitude" e "longitude" no BD

--- lib/repository/db.dart
+++ lib/repository/db.dart
@@ -1,12 +1,12 @@
 import 'package:hasura_connect/hasura_connect.dart';
 
-Future sendPost(double aqi, String geo, String name, String date, double co,
+Future sendPost(double aqi, double latitude, double longitude, String name, String date, double co,
     double no2, double o3, double pm10, double pm25, double so2)async{
   String url = 'https://devportoedu.herokuapp.com/v1/graphql';
   HasuraConnect hasuraConnect = HasuraConnect(url);
   String doc = """
     mutation{
-      insert_localizacoes(objects: {aqi: $aqi, geo: $geo, name: $name, date: $date, co: $co, no2: $no2, o3: $o3, pm10: $pm10, pm25: $pm25, so2: $so2}){
+      insert_localizacoes(objects: {aqi: $aqi, latitude: $latitude, longitude: $longitude, name: $name, date: $date, co: $co, no2: $no2, o3: $o3, pm10: $pm10, pm25: $pm25, so2: $so2}){
         returning {
           id
         }
